85fcc9bb4f8fabbec237180cf709200a6c9f12ce
MateoDelFlow <mateobonanata4@hotmail.com>
2026-06-25 00:41:05 -0300

    feat(storage): agregar dependencia de Glide para carga de imagenes remota

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
